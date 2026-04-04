62d1190cc65d20c6a2c862899ebbe1f66c2f8dcb
Alisher Khairullin <alisherkhairullin463@gmail.com>
2026-04-04 17:50:56 +0500

    microservices

---
+++ ktz-gateway/src/main/java/org/ktz/ktzgateway/KtzGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.ktz.ktzgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtzGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtzGatewayApplication.class, args);
+	}
+
+}
---
+++ ktz-gateway/src/main/java/org/ktz/ktzgateway/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.ktz.ktzgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain security(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
